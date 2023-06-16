8a69d20256276b9abe3d11c4daf59277ccb0f07b
islom <islomjon20010930@gmail.com>
2023-06-17 00:43:24 +0500

    svg rasmlar yuklandi

---
+++ lib/utils/icons.dart
@@ -0,0 +1,9 @@
+class IconsApp {
+  static const arrowBack = 'assets/svg/back_arrow-right.svg';
+  static const bag = 'assets/svg/bag.svg';
+  static const grid = 'assets/svg/grid_menu.svg';
+  static const list = 'assets/svg/list_menu.svg';
+  static const options = 'assets/svg/options.svg';
+  static const plus = 'assets/svg/plus.svg';
+  static const search = 'assets/svg/search.svg';
+}
