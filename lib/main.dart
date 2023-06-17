ae6415c4ec275952f647fd62652838cf3f50b65b
islom <islomjon20010930@gmail.com>
2023-06-17 07:14:23 +0500

    screen 4 bottomsheet tugadi

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:git_tutorial/screens/screen4/screen4.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,25 +12,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home:const Column(
-        children: [
-          Text('Bexruz'),
-          Text('Islomjon'),
-          Text(
-            "ParaDox",
-            style: TextStyle(
-                fontWeight: FontWeight.w600, fontSize: 40, color: Colors.black),
-          ),
-              Text("from abdullayev"),
-              Text("Farrux Madamimov ")
-        ],
-      ),
-    );
+    return ScreenUtilInit(
+        designSize: Size(375, 812),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (context, child) => MaterialApp(
+              title: 'Flutter Demo',
+              theme: ThemeData(
+                colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+                useMaterial3: true,
+              ),
+              home: Screen4(),
+            ));
   }
 }
